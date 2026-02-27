005634d1d872767f9e7d671bbcb0bdeca8afd3ea
sir-phil <phil9icy@gmial.com>
2026-02-27 14:47:29 +0100

    env

--- week1/scraper.py
+++ week1/scraper.py
@@ -1,24 +1,64 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-def fetch_website_contents(url: str) -> str:
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
 
-        soup = BeautifulSoup(response.text, "html.parser")
+# Standard headers to fetch a website
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
 
-        # Remove junk that LLMs don’t need
-        for script in soup(["script", "style", "nav", "footer", "header"]):
-            script.decompose()
 
-        text = soup.get_text(separator="\n")
+def fetch_website_contents(url):
+    """
+    Return the title and contents of the website at the given url;
+    truncate to 2,000 characters as a sensible limit
+    """
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    title = soup.title.string if soup.title else "No title found"
+    if soup.body:
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        text = soup.body.get_text(separator="\n", strip=True)
+    else:
+        text = ""
+    return (title + "\n\n" + text)[:2_000]
 
-        # Clean whitespace
-        lines = [line.strip() for line in text.splitlines() if line.strip()]
-        clean_text = "\n".join(lines)
 
-        return clean_text
+def fetch_website_links(url):
+    """
+    Return the links on the webiste at the given url
+    I realize this is inefficient as we're parsing twice! This is to keep the code in the lab simple.
+    Feel free to use a class and optimize it!
+    """
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = [link.get("href") for link in soup.find_all("a")]
+    return [link for link in links if link]
 
-    except Exception as e:
-        return f"Error fetching website: {str(e)}"
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# def fetch_website_contents(url: str) -> str:
+#     try:
+#         response = requests.get(url, timeout=10)
+#         response.raise_for_status()
+
+#         soup = BeautifulSoup(response.text, "html.parser")
+
+#         # Remove junk that LLMs don’t need
+#         for script in soup(["script", "style", "nav", "footer", "header"]):
+#             script.decompose()
+
+#         text = soup.get_text(separator="\n")
+
+#         # Clean whitespace
+#         lines = [line.strip() for line in text.splitlines() if line.strip()]
+#         clean_text = "\n".join(lines)
+
+#         return clean_text
+
+#     except Exception as e:
+#         return f"Error fetching website: {str(e)}"
+
